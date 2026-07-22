64e9dea9bde9d12d85ca324e7a8f02f481862cae
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-07-22 10:32:06 +0300

    Add files via upload

---
+++ dist/i18n/index.js
@@ -0,0 +1,29 @@
+import ru from './ru.js';
+import vi from './vi.js';
+import en from './en.js';
+import de from './de.js';
+import fr from './fr.js';
+import nl from './nl.js';
+import pl from './pl.js';
+import sv from './sv.js';
+import hu from './hu.js';
+import cs from './cs.js';
+import it from './it.js';
+import pt from './pt.js';
+
+export const FRIDGE_TRANSLATIONS = {
+  ru,
+  vi,
+  en,
+  de,
+  fr,
+  nl,
+  pl,
+  sv,
+  hu,
+  cs,
+  it,
+  pt,
+};
+
+export default FRIDGE_TRANSLATIONS;
